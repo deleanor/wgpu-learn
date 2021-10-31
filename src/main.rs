c785f311167731f65ca06f87061486964ea91701
Eleanor Dicharry <deleanor440@gmail.com>
2021-10-31 11:11:53 -0600

    Show a window.

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use winit::event::Event;
+use winit::event::ElementState;
+use winit::event::WindowEvent;
+use winit::event::KeyboardInput;
+use winit::event::VirtualKeyCode;
+use winit::event_loop::ControlFlow;
+use winit::event_loop::EventLoop;
+use winit::window::WindowBuilder;
+
+fn main() {
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id
+        } if window_id == window.id() => match event {
+            WindowEvent::CloseRequested |
+            WindowEvent::KeyboardInput {
+                input: KeyboardInput {
+                    state: ElementState::Pressed,
+                    virtual_keycode: Some(VirtualKeyCode::Escape),
+                    ..
+                },
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            _ => {}
+        }
+        _ => {}
+    });
+}
